163b17c8acd99b42700f5c12083e89ee5b670d31
Davi Borges <davifelipe_300@hotmail.com>
2023-09-20 15:34:49 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const jsonServer = require('json-server')
+
+const server = jsonServer.create()
+
+const router = jsonServer.router('db.json')
+
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+
+server.use("/api", router)
+
+const porta = process.env.PORT | 3000;
+
+server.listen(porta, () => {
+  console.log(`API NOTEKEEPER ESTA RODADANDO NA PORTA: ${porta}`)
+})
